121f5b6bcac0d02c910e3dedc82c0aa8061a97b2
meron6 <khozameron5@gmail.com>
2025-09-14 12:29:21 +0000

    update

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>Second App Created</Text>
+    <View style={styles.container}>
+      <Text style={styles.largeText}>Entry Screen - Awesome</Text>
+      <Text style={styles.mediumText}>Welcome to your first app!</Text>
+      <Text style={styles.smallText}>Enjoy building with Expo!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  largeText: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  mediumText: {
+    fontSize: 20,
+    fontWeight: "500",
+    marginBottom: 5,
+  },
+  smallText: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: "#666",
+  },
+});
