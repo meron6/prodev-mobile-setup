e22240c6831cc7dc2f5b5563e4226117a8231331
meron6 <khozameron5@gmail.com>
2025-09-14 12:24:03 +0000

    update

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Second App Created</Text>
+    </View>
+  );
+}
